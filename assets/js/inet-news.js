d3488f862eb588f9e2fdebe4374e46775969d4ae
ecstech-admin <james.strommer@ecstech.com>
2025-01-22 14:39:07 -0800

    feat: use webp; rm pngs; rename & update json; rm card border

--- assets/js/inet-news.js
+++ assets/js/inet-news.js
@@ -1,67 +1,106 @@
-var newsData = {
+// JSON objects
+let news1 = {
+    url: "https://news.va.gov/127628/gulf-coast-va-innovators-ideas-to-improve-care/",
+    fellow_status: "current",
+    img: "assets/img/home/ssInnovators-1.webp",
+    alt: "Veteran using virtual reality headset.",
+    title: "VA employees committed to culture aimed at improving Veterans' care",
+    article_copy: "Gulf Coast VA is looking forward to supporting and empowering three VA employees selected for the FY24 VHA Innovators Network (iNET) Spark-Seed-Spread Investment program."
+};
 
-    "data": [
-        {
-            "img": "assets/img/home/ssInnovators-1.png",
-            "alt": "Telehealth logo graphic",
-            "title": "VA Telehealth Services",
-            "description": "The U.S. Department of Veterans Affairs spearheads telehealth innovation, empowering Veterans to access top-tier care conveniently from anywhere through VA Telehealth Services.",
-            "tag": "",
-            "url": "https://telehealth.va.gov/"
-        },
-        {
-            "img": "assets/img/home/ssInnovators-2.jpg",
-            "alt": "Man holding USA flag leading a group of people walking on the street",
-            "title": "Veterans Explore Cincinnati VA's Whole Health Challenge",
-            "description": "Veterans participate in a 10-week Whole Health Challenge focused on promoting camaraderie and wellness through community engagement, personalized health plans, and shared experiences.",
-            "tag": "VA's Internal Network",
-            "url": "https://vaww.blog.va.gov/CCM/?p=6655"
-        },
-        {
-            "img": "assets/img/home/ssInnovators-3.png",
-            "alt": "HELPP logo graphic",
-            "title": "HELPP Delivers Timely Veteran Care",
-            "description": "Dr. Nichol Salvo, deputy director of VA's National Podiatry Program, spearheaded the High-risk Eye and Limb Preservation Program (HELPP) during the pandemic, reducing amputation rates by 40% and wait times from 38 to 10 days at Atlanta VA",
-            "tag": "",
-            "url": "https://news.va.gov/128989/innovation-reduce-amputations-veterans-walking/"
-        },
-        {
-            "img": "assets/img/home/ssInnovators-4.jpg",
-            "alt": "A woman standing and laptop showing feet sensor display",
-            "title": "Remote Temperature Monitoring Saves Veterans' Limbs and Lives",
-            "description": "Uncover the transformative impact of VA's pioneering remote temperature monitoring program, enriching Veteran well-being by significantly enhancing the detection of illnesses that precipitate limb loss.",
-            "tag": "",
-            "url": "https://news.va.gov/125818/connecting-veterans-to-diabetes-care-telehealth/"
-        } 
-    ]                                         
+let news2 = {
+    url: "https://news.va.gov/119569/collaboration-continues-greenhouse-initiative/",
+    fellow_status: "current",
+    img: "assets/img/home/ssInnovators-2.webp",
+    alt: "Man being helped by nurse.",
+    title: "VA Innovation continues through Greenhouse Initiative",
+    article_copy: "One of VHA Innovators Network's (iNET) signature programs is the Greenhouse Initiative, an opportunity for the health care innovation community to collaborate at the earliest stages of innovation."
+};
+
+let news3 = {
+    url: "https://news.va.gov/126123/va-awards-for-veteran-care-innovators-network/",
+    fellow_status: "current",
+    img: "assets/img/home/ssInnovators-3.webp",
+    alt: "Group of people on stage",
+    title: "VA employees recognized at VA Innovation Experience event",
+    article_copy: "VA has a long history of innovation powered by employees who demonstrate dedication to their work and resilience in the face of adversity to enable better Veteran care."
+};
+
+let news4 = {
+    url: "https://news.va.gov/115102/gulf-coast-employees-innovators-network/",
+    fellow_status: "current",
+    img: "assets/img/home/ssInnovators-4.webp",
+    alt: "Woman with VHA Innovators project",
+    title: "VA Gulf Coast employees advance in VHA Innovators Network",
+    article_copy: "VA Gulf Coast Healthcare System to advance as part of iNET's innovation investment program."
+};
+
+
+// Mustache render logic for individual page views | ?user="firstname-lastname"
+var fellowsArray = new Array(news1, news2, news3, news4);
+
+var tab;
+var img;
+var alt;
+var title;
+var article_copy;
+
+for (var i = 0; i < fellowsArray.length; i++) {
+    var userIndex = fellowsArray[i].url;
+    if (window.location.href.indexOf(userIndex) > -1) {
+
+        var objectView = fellowsArray[i];
+
+        img = Mustache.render("{{{img}}}", objectView);
+        alt = Mustache.render("{{alt}}", objectView);
+        bio_name = Mustache.render("{{bio_name}}", objectView);
+        title = Mustache.render("{{title}}", objectView);
+        article_copy = Mustache.render("{{{article_copy}}}", objectView);
+
+    }
 }
 
 
-// render dynamic gallery data
-//newsData.data.forEach(news => $('.owl-carousel.usa-card-group').append("<div class='usa-card__container item'>" + "<a class='card-group-tags' href='" + news.url + "'>" + "<div class='usa-card__media' >" + "<div class='usa-card__img'>" + "<img  src='" + news.img + "'alt='" + news.alt + "' />" + "</div>" + "</div>" + "<div class='usa-card__body'>" + "<h2 class='h3'>" + news.title + "</h2>" + "<p>" + news.description + "</p>" + "<span class='usa-tag' style='background-color: #FFBE2E !important; text-transform:none; color: #1B1B1B'>" + news.tag + "</span>" + "</div>" + "</a>" + "</div>")); 
+// Append json data to html elements
+$('head title').text(tab);
+$('.bio-card-img > img').attr("alt", alt);
+$('.bio-card-attributes > h2').text(title);
+$('.bio-card-attributes > p span.article_copy').text(article_copy);
 
-// Render dynamic gallery data
-newsData.data.forEach(news => {
-    $('.owl-carousel.usa-card-group').append(
-      `<div class="usa-card__container item">
-        <a class="card-group-tags" href="${news.url}">
-          <div class="usa-card__media">
-            <div class="usa-card__img">
-              <img src="${news.img}" alt="${news.alt}" />
-            </div>
-          </div>
-          <div class="usa-card__body">
-            <h2 class="h3">${news.title}</h2>
-            <p>${news.description}</p>
-            <span class="usa-tag text-base-darkest bg-accent-warm text-no-uppercase">${news.tag}</span>
-          </div>
-        </a>
-      </div>`
-    );
-  });
+$(".bio-card-attributes p span:empty").remove();
+$(".bio-card-attributes p:empty").remove();
+
+if ($(".bio-card-attributes > p.margin-top-0").text() == ' ') {
+    $(".bio-card-attributes > p.margin-top-0").remove();
+}
+
+if ($(".bio-card-attributes > p.margin-bottom-0").text() == ' ') {
+    $(".bio-card-attributes > p.margin-bottom-0").remove();
+}
+
+$("#bio-card-about p:empty").remove();
 
 
-// remove empty elements
-$(".usa-card__body h4:empty").remove();
-$(".usa-card__body p:empty").remove();
-$(".usa-card__body span.usa-tag:empty").remove();
+// Render dynamic owl carousel for CURRENT sif & erf fellows
+var currentFellowsArray = fellowsArray;
+var newCurrentFellowsArray = [];
+
+for (var i = 0; i < currentFellowsArray.length; i++) {
+    if (currentFellowsArray[i].fellow_status == "current") {
+        newCurrentFellowsArray.push(currentFellowsArray[i]);
+        // console.log(newCurrentFellowsArray);
+    }
+}
+
+var currentData = {
+    data: []
+};
+
+currentData.data.push(newCurrentFellowsArray);  
+
+currentData.data[0].forEach(current => $('.owl-carousel.nci2i--news').append("<div class='usa-card__container item'>" + "<a href=" 
++ current.url + " />" + "<div class='usa-card__media' >" + "<div class=''>" + "<img src='" 
++ current.img + "'alt='" + current.alt + "' />" + "</div>" + "</div>" 
++ "<div class='usa-card__body'>" + "<h3 class='margin-y-0'>" 
++ current.title + "</h3>" + "<p>" 
++ current.article_copy + "</p>" + "</div>" + "</a>" + "</div>"));
