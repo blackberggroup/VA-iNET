ed110f3f1b5bfcef84599297ba279ac96aa14fa7
ecstech-admin <james.strommer@ecstech.com>
2025-02-04 11:51:50 -0800

    feat: update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Define the Fellows collection
+  eleventyConfig.addCollection("product", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("product/*.md");
+  });
+
   let pathPrefix = "/";
 
   if (process.env.BASEURL) {
